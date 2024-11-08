735e68f35bdbe96161bce5f4de4084f75fc34777
neo-0007 <hrishikeshgohain123@gmail.com>
2024-11-09 04:11:12 +0530

    feat: complete home screen ui

--- frontend_mobile/lib/features/home/view/screens/home_screen.dart
+++ frontend_mobile/lib/features/home/view/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend_mobile/features/common/widgets/big_text.dart';
+import 'package:frontend_mobile/features/home/view/widgets/daily_dosage_container.dart';
+import 'package:frontend_mobile/features/home/view/widgets/home_page_carousal.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,15 +9,43 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body:Padding(padding:EdgeInsets.symmetric(horizontal: 30, vertical: 40),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          BigText(text: 'Prescriptrix')
-        ],
-      )
-    )
+      backgroundColor: const Color.fromARGB(255, 232, 230, 230),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const BigText(
+                text: 'Prescriptrix',
+                fontSize: 25,
+                color: Color.fromARGB(255, 0, 89, 255),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const HomePageCarousal(),
+              SizedBox(
+                height: 20,
+              ),
+              BigText(text: 'Daily Dosages'),
+              SizedBox(
+                height: 10,
+              ),
+              DailyDosageContainer(),
+              SizedBox(
+                height: 5,
+              ),
+              DailyDosageContainer(),
+              SizedBox(
+                height: 5,
+              ),
+              DailyDosageContainer(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
