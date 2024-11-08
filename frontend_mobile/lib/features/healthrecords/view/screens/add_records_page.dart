a97f488552f87c08d7354e6a1352b7e4fd0754b1
neo-0007 <hrishikeshgohain123@gmail.com>
2024-11-09 00:15:04 +0530

    feat: complete upload report page ui

--- frontend_mobile/lib/features/healthrecords/view/screens/add_records_page.dart
+++ frontend_mobile/lib/features/healthrecords/view/screens/add_records_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_mobile/features/healthrecords/view/widgets/health_record_button.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AddRecordsPage extends StatefulWidget {
@@ -12,16 +13,52 @@ class _AddRecordsPageState extends State<AddRecordsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text(
-            'Add Record',
-            style: GoogleFonts.poppins(
-                fontSize: 27.25,
-                fontWeight: FontWeight.bold,
-                color: Colors.black),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Add Record',
+                  style: GoogleFonts.poppins(
+                      fontSize: 27.25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
+                onTap: () {},
+                child: Container(
+                  
+                  height: 250,
+                  width: MediaQuery.of(context).size.width - 100,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(20, 0, 98, 255),
+                    border: Border.all(width: 2, color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Center(
+                    child: Icon(Icons.note_add_rounded),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            HealthRecordButton(buttonText:'Upload', onPressed: (){})
+          ],
+        ),
       ),
     );
   }
