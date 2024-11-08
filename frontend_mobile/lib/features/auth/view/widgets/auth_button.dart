d8a7a489ea082f4befe2049d710be035f6f740d3
neo-0007 <hrishikeshgohain123@gmail.com>
2024-11-08 18:09:34 +0530

    feat: add button text and textcolor as argument

--- frontend_mobile/lib/features/auth/view/widgets/auth_button.dart
+++ frontend_mobile/lib/features/auth/view/widgets/auth_button.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AuthButton extends StatelessWidget {
-  const AuthButton({super.key, required this.buttonText, required this.onPressed});
+  const AuthButton(
+      {super.key,
+      required this.buttonText,
+      required this.onPressed,
+      this.buttonColor= const Color.fromARGB(255, 0, 98, 255),this.buttonTextColor= Colors.white});
 
   final String buttonText;
   final void Function() onPressed;
+  final Color buttonColor;
+  final Color buttonTextColor ;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +20,7 @@ class AuthButton extends StatelessWidget {
       height: 50,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor:const Color.fromARGB(255,0,98,255),
+          backgroundColor: buttonColor,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8),
           ),
@@ -23,7 +29,7 @@ class AuthButton extends StatelessWidget {
         child: Text(
           buttonText,
           style: GoogleFonts.poppins(
-              color: Colors.white, fontSize: 15, fontWeight: FontWeight.w600),
+              color: buttonTextColor, fontSize: 15, fontWeight: FontWeight.w600),
         ),
       ),
     );
