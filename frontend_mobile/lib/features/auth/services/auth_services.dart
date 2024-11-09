2943e42b8735738ce4a972113a541a2ac06c7852
neo-0007 <hrishikeshgohain123@gmail.com>
2024-11-09 23:06:41 +0530

    feat:use shared pref to store jwt token

--- frontend_mobile/lib/features/auth/services/auth_services.dart
+++ frontend_mobile/lib/features/auth/services/auth_services.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:frontend_mobile/configs/api_constants.dart';
 import 'package:frontend_mobile/features/auth/models/user_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthServices {
   Future<String> signup(UserModel user) async {
@@ -31,4 +32,19 @@ class AuthServices {
       return res.body.toString();
     }
   }
+
+  Future<void> storeToken(String token) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('token', token);
+  }
+
+  Future<String?> getToken() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  return prefs.getString('token');
+}
+
+Future<void> clearToken() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.remove('token');
+}
 }
