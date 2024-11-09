d01435b3420a135253964519fc6e2cd137edbb8f
neo-0007 <hrishikeshgohain123@gmail.com>
2024-11-10 05:15:59 +0530

    feat: add all endpoints to apiconstants config

--- frontend_mobile/lib/configs/api_constants.dart
+++ frontend_mobile/lib/configs/api_constants.dart
@@ -1,9 +1,11 @@
 class ApiConstants {
   // ignore: constant_identifier_names
   static const String BASE_URL = 'http://localhost:3000';
-  static const String addPrescription = '$BASE_URL/api/v1/add-prescription';
-  static const String addLabReports = '$BASE_URL/api/v1/add-labreports';
-  static const String signup = '$BASE_URL/api/v1/signup';
-  static const String login = '$BASE_URL/api/v1/login';
-
+  static const String addPrescription = '$BASE_URL/api/v1/user/upload-prescription';
+  static const String addLabReports = '$BASE_URL/api/v1/user/upload-lab-report';
+  static const String getAllPrescriptions = '$BASE_URL/api/v1/user/prescription/all';
+  static const String getPrescription = '$BASE_URL/api/v1/user/prescription/byId';
+  static const String signup = '$BASE_URL/api/v1/user/signup';
+  static const String login = '$BASE_URL/api/v1/user/login';
+  static const String verifyToken = '$BASE_URL/api/v1/user/verify';
 }
