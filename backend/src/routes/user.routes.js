9796d9db3657559cf65d600c79057a543b73c04b
dhritiman78 <dhritiman.saikia.11b.244@gmail.com>
2024-11-09 23:11:03 +0530

    added login route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,12 +2,14 @@ import express from "express";
 import { upload } from "../middleware/multer.middleware.js";
 // import path from "path";
 
-import { uploadPrescription, userSignUp } from "../controllers/user.controller.js";
+import { uploadPrescription, userSignUp,userLogIn } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 // Define the image upload route
 router.post("/signup", userSignUp);
+router.post("/login", userLogIn);
+
 router.post("/upload", upload.single("image"), uploadPrescription);
 
 export default router;
