458d7f6ba4c283aa9dcf8c040ecdaa3f6dfdab5f
neo-0007 <hrishikeshgohain123@gmail.com>
2024-11-09 20:29:08 +0530

    feat:create profile page ui

--- frontend_mobile/lib/features/common/widgets/small_text.dart
+++ frontend_mobile/lib/features/common/widgets/small_text.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SmallText extends StatelessWidget {
-  const SmallText({super.key, required this.text});
+  const SmallText({super.key, required this.text,this.color=Colors.black});
 
   final String text;
-
+final Color color;
   @override
   Widget build(BuildContext context) {
     return Text(
